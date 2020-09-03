cec8f723652622b2f19a4fe63fb0ca8e5bd56480
Anthony Jimenez <anthonyjim96@gmail.com>
2020-09-03 01:02:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ def monopoly_with_third_tier
     :four_pieces_owned => 200
   }
 
-  railroads[:railroads][:name] = {
+  railroads[:railroads][:names] = {
     :reading_railroad => {},
     :pennsylvania_railroad => {},
     :b_and_o_railroad =>  {},
